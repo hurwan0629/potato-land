5f88a5849a98cb6023ff62e1c6c773bc9644c234
Shim-HyoungJoon <gudwns15963@gmail.com>
2026-08-04 17:14:06 +0900

    feat: 채팅, 알림 1차

--- backend/src/modules/reviews/reviews.router.js
+++ backend/src/modules/reviews/reviews.router.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
 
-<<<<<<< Updated upstream
-import { createReview, listUserReviews } from "./reviews.controller.js";
-=======
 import {
   createReview,
   listReviewTags,
 } from "./reviews.controller.js";
->>>>>>> Stashed changes
 
 export const reviewsRouter = Router();
 
